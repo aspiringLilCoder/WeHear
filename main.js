6b6ccc90c7314bbe7d5fd986dde6d2634c7d52a7
aoi.coder <87404668+aspiringLilCoder@users.noreply.github.com>
2024-01-08 03:05:34 +0900

    getElementById statements js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const listenTab = document.getElementById("listen_tab");
+    const aboutTab = document.getElementById("about_tab");
+    const settingsTab = document.getElementById("settings_tab");
+
+    const listenLink = document.getElementById("listen_link");
+    const aboutLink = document.getElementById("about_link");
+    const settingsLink = document.getElementById("settings_link");
+
+    const listenButton = document.getElementById("listen");
+
+    const addSoundButton = document.getElementById("add_sound_btn");
+    const soundList = document.getElementById("soundList");
+    const alertInput = document.getElementById("alert_input");
+});
