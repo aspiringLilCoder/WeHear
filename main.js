af9ecaa2436050cfbd48e82f142c9f301473a0e3
aoi.coder <87404668+aspiringLilCoder@users.noreply.github.com>
2024-01-08 03:45:36 +0900

    tab navigation js + css fixes

--- main.js
+++ main.js
@@ -10,6 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const listenButton = document.getElementById("listen");
 
     const addSoundButton = document.getElementById("add_sound_btn");
-    const soundList = document.getElementById("soundList");
+    const soundList = document.getElementById("sound_list");
     const alertInput = document.getElementById("alert_input");
+
+    listenLink.addEventListener("click", () => selectTab(listenTab));
+    aboutLink.addEventListener("click", () => selectTab(aboutTab));
+    settingsLink.addEventListener("click", () => selectTab(settingsTab));
+
+
+    const tabs = [listenTab, aboutTab, settingsTab];
+    const navLinks = [listenLink, aboutLink, settingsLink];
+
+    listenTab.style.display = "flex";
+    navLinks[0].classList.add("selected");
+
+    aboutTab.style.display = "none";
+    settingsTab.style.display = "none";
+
+    function selectTab(tab) {
+        for (let i = 0; i < tabs.length; i++) {
+            const el = tabs[i];
+            el.style.display = "none";
+            navLinks[i].classList.remove("selected");
+            if (el === tab) {
+                el.style.display = "flex";
+               navLinks[i].classList.add("selected");
+            }
+        }
+    }
+
 });
